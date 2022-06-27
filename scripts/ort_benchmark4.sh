457cc0c7b5e6d251a7f12abcb1a4da7a48f70425
Vidoje Gavrilovic <vidoje.gavrilovic@htecgroup.com>
2022-06-27 16:22:35 +0200

    update benchmark4 script

--- scripts/ort_benchmark4.sh
+++ scripts/ort_benchmark4.sh
@@ -12,56 +12,36 @@ popd
 cd ${EXEDIR}
 touch ${testdir}/summary.csv
 
-# Ideal batch: 1,2,4,8,16,32,64,128,256
-# Ideal sequence: 16,32,64,128,256,384,512
-echo "\n***** bert-base-cased migraphx\n" >> $testdir/dashboard.out
-echo "\n***** bert-base-cased migraphx\n" >> $testdir/dashboard.err
-python benchmark.py -g -m bert-base-cased --sequence_length 32 384 --batch_sizes 1 32 --provider=migraphx -p fp16 --disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu --result_csv $testdir/dashboard-ep.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
-
-# Ideal batch: 1,2,4,8,16,32,64,128
-# Ideal sequence: 16,32,64,128,256,384,512
-echo "\n***** bert-large-uncased migraphx\n" >> $testdir/dashboard.out
-echo "\n***** bert-large-uncased migraphx\n" >> $testdir/dashboard.err
-python benchmark.py -g -m bert-large-uncased --sequence_length 32 384 --batch_sizes 1 16 --provider=migraphx -p fp16 --disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu --result_csv $testdir/dashboard-ep.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
-
-# Ideal batch: 1,2,4,8,16,32
-# Ideal sequence: 16,32,64,128,256,384,512
-echo "\n***** distilgpt2 migraphx\n" >> $testdir/dashboard.out
-echo "\n***** distilgpt2 migraphx\n" >> $testdir/dashboard.err
-python benchmark.py -g -m distilgpt2 --model_class AutoModelForCausalLM --sequence_length 32 384 --batch_sizes 1 8 --provider=migraphx -p fp16 --disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu --result_csv $testdir/dashboard-ep.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
-
 # clear the cached optimized models
 rm ./onnx_models/*gpu*
 
-# Ideal batch: 1,2,4,8,16,32,64,128,256
-# Ideal sequence: 16,32,64,128,256,384,512
-echo "\n***** bert-base-cased migraphx engine\n" >> $testdir/dashboard.out
-echo "\n***** bert-base-cased migraphx engine\n" >> $testdir/dashboard.err
-python benchmark.py -g -m bert-base-cased --sequence_length 32 384 --batch_sizes 1 32 --engine=migraphx -p fp16 --disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu --result_csv $testdir/dashboard-eng.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
+while read model bs
+do
+    echo "\n ***** " ${model} "migraphx engine\n"
+    python benchmark.py -g -m  ${model} --sequence_length 32 384 --batch_sizes 1 ${bs} --engine=migraphx -p fp16 --disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu --result_csv $testdir/dashboard-eng.csv --detail_csv $testdir/dashboard-detail.csv
 
-# Ideal batch: 1,2,4,8,16,32,64,128
-# Ideal sequence: 16,32,64,128,256,384,512
-echo "\n***** bert-large-uncased migraphx engine\n" >> $testdir/dashboard.out
-echo "\n***** bert-large-uncased migraphx engine\n" >> $testdir/dashboard.err
-python benchmark.py -g -m bert-large-uncased --sequence_length 32 384 --batch_sizes 1 16 --engine=migraphx -p fp16 --disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu --result_csv $testdir/dashboard-eng.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
+    rm ./onnx_models/*gpu*
+    echo "\n ***** " ${model} "rocm\n"
+    python benchmark.py -g -m  ${model} --sequence_length 32 384 --batch_sizes 1 ${bs} --provider=rocm -p fp16 --result_csv $testdir/dashboard-rocm.csv --detail_csv $testdir/dashboard-detail.csv
 
-# Ideal batch: 1,2,4,8,16,32
-# Ideal sequence: 16,32,64,128,256,384,512
-echo "\n***** distilgpt2 migraphx engine\n" >> $testdir/dashboard.out
-echo "\n***** distilgpt2 migraphx engine\n" >> $testdir/dashboard.err
-python benchmark.py -g -m distilgpt2 --model_class AutoModelForCausalLM --sequence_length 32 384 --batch_sizes 1 8 --engine=migraphx -p fp16 --disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu --result_csv $testdir/dashboard-eng.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
-
-# clear the cached optimized models
-rm ./onnx_models/*gpu*
-
-echo "\n***** bert-base-cased rocm\n" >> $testdir/dashboard.out
-echo "\n***** bert-base-cased rocm\n" >> $testdir/dashboard.err
-python benchmark.py -g -m bert-base-cased --sequence_length 32 384 --batch_sizes 1 32 --provider=rocm -p fp16 --result_csv $testdir/dashboard-rocm.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
-
-echo "\n***** bert-large-uncased rocm\n" >> $testdir/dashboard.out
-echo "\n***** bert-large-uncased rocm\n" >> $testdir/dashboard.err
-python benchmark.py -g -m bert-large-uncased --sequence_length 32 384 --batch_sizes 1 16 --provider=rocm -p fp16 --result_csv $testdir/dashboard-rocm.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
-
-echo "\n***** distilgpt2 rocm\n" >> $testdir/dashboard.out
-echo "\n***** distilgpt2 rocm\n" >> $testdir/dashboard.err
-python benchmark.py -g -m distilgpt2 --model_class AutoModelForCausalLM --sequence_length 32 384 --batch_sizes 1 8 --provider=rocm -p fp16 --result_csv $testdir/dashboard-rocm.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
+    rm ./onnx_models/*gpu*
+    echo "\n ***** " ${model} "migrpahx ex provider\n"
+    python benchmark.py -g -m  ${model} --sequence_length 32 384 --batch_sizes 1 ${bs} --provider=migraphx -p fp16 --result_csv $testdir/dashboard-ep.csv --disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu --detail_csv $testdir/dashboard-detail.csv
+done <<BMARK_LIST
+distilgpt2 8
+bert-base-uncased 32
+openai-gpt 8
+gpt2 8
+roberta-large 8
+distilroberta-base 32
+deepset/roberta-base-squad2 32
+bert-base-cased 32
+bert-large-uncased 16
+roberta-base 16
+deepset/roberta-base-squad2 32
+camembert-base 32
+albert-base-v1 32
+albert-base-v2 32
+xlm-roberta-base 16
+microsoft/layoutlm-base-uncased 32
+BMARK_LIST
